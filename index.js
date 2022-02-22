1c1e53c18b65eca7363dac13846083951b29d282
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2022-02-21 23:35:31 -0300

    criando função para ler o arquivo de texto

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 import chalk from 'chalk';
+import fs from 'fs';
 
-console.log(chalk.blue('Teste'));
+pegaArquivo('./arquivos/texto1.md');
+
+function pegaArquivo(caminhoDoArquivo) {
+  const encoding = 'utf-8';
+  fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
+    console.log(chalk.green(texto));
+  })
+}
