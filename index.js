dbccfbcdf70d2682be0fe85849e801ef62c45594
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2022-04-03 19:20:38 -0300

    transformando o método de resgatar o arquivo assíncrono

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 import chalk from 'chalk';
 import fs from 'fs';
 
-pegaArquivo('./arquivos/');
+pegaArquivo('./arquivos/texto1.md');
+
 
 function trataErro(erro) {
-  throw new Error(chalk.red(erro));
+  throw new Error(chalk.red(erro.code, 'Não há arquivo no caminho'));
 }
 
 function pegaArquivo(caminhoDoArquivo) {
   const encoding = 'utf-8';
-  fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-    if(erro) {
-      trataErro(erro);
-    }
-    console.log(chalk.green(texto));
-  });
+  fs.promises.readFile(caminhoDoArquivo, encoding)
+  .then((sucesso) => console.log(sucesso))
+  .catch((error) => trataErro(error))
 }
 
-pegaArquivo('/arquivos/');
+// function pegaArquivo(caminhoDoArquivo) {
+//   const encoding = 'utf-8';
+//   fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//     if(erro) {
+//       trataErro(erro);
+//     }
+//     console.log(chalk.green(texto));
+//   });
+// }
