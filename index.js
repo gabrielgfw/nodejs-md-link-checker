b99a7812c3f9de5b0aa7a630eb57e1af04e62ea5
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2022-04-03 19:11:49 -0300

    adicionando tratamento de erro para a função de resgatar arquivo

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import chalk from 'chalk';
 import fs from 'fs';
 
-pegaArquivo('./arquivos/texto1.md');
+pegaArquivo('./arquivos/');
+
+function trataErro(erro) {
+  throw new Error(chalk.red(erro));
+}
 
 function pegaArquivo(caminhoDoArquivo) {
   const encoding = 'utf-8';
-  fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
+  fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+    if(erro) {
+      trataErro(erro);
+    }
     console.log(chalk.green(texto));
-  })
+  });
 }
+
+pegaArquivo('/arquivos/');
