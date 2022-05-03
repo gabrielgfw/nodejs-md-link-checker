2a99f8dda9db7a3375b79b3d01cd281de94cf220
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2022-05-03 20:14:16 -0300

    add regex para extrair os links do arquivo texto

--- index.js
+++ index.js
@@ -1,37 +1,31 @@
 import chalk from 'chalk';
 import fs from 'fs';
 
-pegaArquivo('./arquivos/texto12.md');
+pegaArquivo('./arquivos/texto1.md');
 
+
+function extraiLinks(texto) {
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+  const arrayResultados = [];
+  let temp;
+
+  while((temp = regex.exec(texto)) !== null) {
+    arrayResultados.push({ [temp[1]]: temp[2] });
+  }
+  return arrayResultados;
+}
 
 function trataErro(erro) {
   throw new Error(chalk.red(erro.code, 'Não há arquivo no caminho'));
 }
 
-
 async function pegaArquivo(caminhoDoArquivo) {
   const encoding = 'utf-8';
   try {
     const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-    console.log(chalk.yellow(texto));
+    const linksExtraidos = extraiLinks(texto);
+    console.log(chalk.yellow(JSON.stringify(linksExtraidos)));
   } catch(error) {
     trataErro(error);
   }
 }
-
-// function pegaArquivo(caminhoDoArquivo) {
-//   const encoding = 'utf-8';
-//   fs.promises.readFile(caminhoDoArquivo, encoding)
-//   .then((sucesso) => console.log(sucesso))
-//   .catch((error) => trataErro(error))
-// }
-
-// function pegaArquivo(caminhoDoArquivo) {
-//   const encoding = 'utf-8';
-//   fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-//     if(erro) {
-//       trataErro(erro);
-//     }
-//     console.log(chalk.green(texto));
-//   });
-// }
